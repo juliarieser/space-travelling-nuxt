c4a4660c0e139ed84a802eb421cfb970529025ea
juliarieser <79445370+juliarieser@users.noreply.github.com>
2022-03-11 16:19:48 +0100

    technology getting data

--- pages/technology.vue
+++ pages/technology.vue
@@ -1,4 +1,93 @@
-/* eslint-disable vue/multi-word-component-names */
 <template>
-  <h1>Technology</h1>
+  <div class="container flow flex technology">
+    <h1 class="numbered-title"><span>03</span> Space Launch 101</h1>
+    <div id="content" v-if="technology">
+      <img :src="technology['image-landscape'].guid" />
+      <ul class="dot-indicators flex">
+        <li
+          v-for="(item, index) in technologies"
+          :key="item['technology-name']"
+          @click="setTechnology(index)"
+          class="tech-number"
+        >
+          {{ index + 1 }}
+        </li>
+      </ul>
+      <div class="vehicle-info">
+        <h3>The Terminology ...</h3>
+
+        <h2>{{ technology['technology-name'] }}</h2>
+        <p>{{ technology.description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      technologies: [],
+      activeTechnologyIndex: 2,
+    }
+  },
+  methods: {
+    async loadData() {
+      const response = await fetch(
+        'https://space.wp.seiwald.hak-stjo.schulwebspace.at/wp-json/wp/v2/technology'
+      )
+      this.technologies = await response.json()
+    },
+    setTechnology(index) {
+      this.activeTechnologyIndex = index
+    },
+  },
+  mounted() {
+    this.loadData()
+    document.body.style.backgroundImage =
+      "url('background-technology-mobile.jpg')"
+  },
+  computed: {
+    technology() {
+      return this.technologies[this.activeTechnologyIndex]
+    },
+  },
+}
+</script>
+
+<style scoped>
+.technology {
+  flex-direction: column;
+  align-items: center;
+}
+.vehicle-info {
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+img {
+  width: 100vw;
+  position: relative;
+  left: 50%;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
+}
+
+.numbered-title {
+  font-size: 16px;
+}
+
+.dot-indicators {
+  list-style-type: none;
+  justify-content: center;
+  margin-left: -15vw;
+}
+
+.tech-number {
+  padding: 10px 20px 10px 20px;
+
+  border: solid white 1px;
+  background: none;
+}
+</style>
